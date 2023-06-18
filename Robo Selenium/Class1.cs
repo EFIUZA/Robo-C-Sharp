75793cc07f4cd0b850e803356ed0d9aa265efd17
EFIUZA <128154992+EFIUZA@users.noreply.github.com>
2023-06-18 04:46:45 -0300

    first commit

---
+++ Robo Selenium/Class1.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Robo_Selenium
+{
+    public class AutomationWeb
+    {
+        public IWebDriver Driver;
+        public AutomationWeb()
+        {
+            Driver = new EdgeDriver();
+        }
+        public void TestWeb()
+        {
+            Driver.Navigate().GoToUrl("https://www.google.com");
+            Driver.FindElement(By.Name("q")).SendKeys("teste");
+            Driver.FindElement(By.Name("q")).SendKeys(Keys.Enter);
+            Console.ReadLine();
+            Driver.Quit();
+
+        }
+
+    }
+}
---
+++ Robo Selenium/Program.cs
@@ -0,0 +1,5 @@
+﻿using Robo_Selenium;
+using static System.Net.Mime.MediaTypeNames;
+
+var web = new AutomationWeb();
+web.TestWeb();
